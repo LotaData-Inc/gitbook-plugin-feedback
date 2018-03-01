34668ab3cbea6933d98bd064a6bbcd6535c92c0c
Felipe Thomaz Pedroni <felipe@lotadata.com>
2018-03-01 17:04:17 -0300

    Fix feedback color

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
         // called on each book & each language book
         'init': function() {
             var cfg = this.config.get('pluginsConfig.page-feedback'), _this = this;
-            elementStyle = 'border: #483D68 2px solid;border-radius: 5px;padding: 5px;margin-left: 10px;color: #483D68;'
+            elementStyle = 'border: #05022c 2px solid;border-radius: 5px;padding: 5px;margin-left: 10px;color: #05022c;'
             feedbackString = '<strong>Was this helpful?</strong> <span id="page-feedback-buttons"><a id="page-feedback-yes" href="#" style="' + elementStyle + '">Yes, thanks!</a> <a id="page-feedback-no" href="#" style="' + elementStyle + '">Not really</a></span>';
         },
         'page:before': function(page) {
